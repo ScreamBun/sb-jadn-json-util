8279629fc4aa36c2c4daa544a87bff6018191f00
Matt Roberts <matthew.roberts@bestgateeng.com>
2024-02-16 09:08:05 -0500

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name="jadn-json",
+    version="1.0.0", 
+    packages=["jadnjson", "jadnjson.generator", "jadnjson.constants", "jadnjson.helpers", "jadnjson.utils", "jadnjson.validation"],
+    install_requires=[
+
+    ]    
+)
