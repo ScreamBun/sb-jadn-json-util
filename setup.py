5cf1c8382980707b6db5501be9a0e8337dc05a25
Matt Roberts <matthew.roberts@bestgateeng.com>
2024-08-26 15:35:52 -0400

    bumped whl to 1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name="jadnjson",
-    version="1.2.0", 
+    version="1.3.0", 
     packages=["jadnjson", "jadnjson.generators", "jadnjson.constants", "jadnjson.utils", "jadnjson.validators"],
     install_requires=[
         "Faker",
