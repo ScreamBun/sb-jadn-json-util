1da84aeab48bba1acea3867669b97c299c3d3ef9
Matt Roberts <matthew.roberts@bestgateeng.com>
2024-09-10 10:23:02 -0400

    cleanup

--- jadnjson/validators/schema_validator.py
+++ jadnjson/validators/schema_validator.py
@@ -1,16 +1,10 @@
-import json
 from jsonschema import Draft202012Validator
-# from jsonschema import Draft201909Validator
 
 
 def validate_schema(schema: dict)-> tuple[bool, str]:
     #TODO: validate by draft $schema
-    
-    # json_object = json.dumps(schema, indent = 4)     
 
     try:
-        # Draft201909Validator.check_schema(schema)
-        # Draft202012Validator.check_schema(json_object)
         Draft202012Validator.check_schema(schema)
         return True, "Schema is Valid"
 
